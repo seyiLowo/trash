244fd06442b205da69341fada1f00ac438dd995e
nimwin <seyilowo@gmail.com>
2019-09-13 13:05:10 +0100

    initial commit

---
+++ ShoppingAPI/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingAPI.Models;
+
+namespace ShoppingAPI.Controllers
+{
+    [Route("products/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        Product[] products = new Product[]
+        {
+            new Product {ProductID = 1, Name = "Samsung S10", Category = "Mobile Phones", Price = 3000},
+            new Product {ProductID = 2, Name = "Samsung S10", Category = "Mobile Phones", Price = 2999.99M},
+            new Product {ProductID = 3, Name = "Samsung S10", Category = "Mobile Phones", Price = 2999},
+            new Product {ProductID = 4, Name = "Samsung S10", Category = "Mobile Phones", Price = 3200}
+        };
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return products;
+        }
+    }
+}
